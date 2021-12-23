c53256f8afee6dd834a36f0120cfea6e99025fb4
tina <53224438+xiao-ma-nong@users.noreply.github.com>
2021-12-23 22:43:28 +0800

    新增功能

--- src/components/Tina.vue
+++ src/components/Tina.vue
@@ -12,7 +12,7 @@
                             </el-button>
                         </div>
                         <div class="text item">
-                            <img src="http://tinaroot.cn/static/picture/logo.png"/>
+                            <img src="https://ae02.alicdn.com/kf/Ha19e38d658834a19ba29a90851517849q.png"/>
                         </div>
                     </el-card>
                 </a>
@@ -21,13 +21,47 @@
                 <a href="http://lottery.tinaroot.cn" target="_blank">
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
-                            <span>Tina九宫格抽奖</span>
+                            <span>九宫格抽奖</span>
                             <el-button style="float: right; padding: 3px 0" type="text">
                                 跳转
                             </el-button>
                         </div>
                         <div class="text item">
-                            <img src="https://ae04.alicdn.com/kf/H73f2326157ad4e9fbc8c7de7d432830cb.png"/>
+                            <img src="https://ae01.alicdn.com/kf/H8b39c76b0a05462aac2eec7760a4e91c7.png"/>
+                        </div>
+                    </el-card>
+                </a>
+            </el-col>
+        </el-row>
+        <p></p>
+        <el-row>
+            <el-col :span="8" :offset="4">
+                <a href="http://admin.tinaroot.cn" target="_blank">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>后台管理系统</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">
+                                跳转
+                            </el-button>
+                        </div>
+                        <div class="text item">
+                            <img src="https://ae01.alicdn.com/kf/H8b39c76b0a05462aac2eec7760a4e91c7.png"/>
+                        </div>
+                    </el-card>
+                </a>
+            </el-col>
+
+            <el-col :span="8" :offset="3">
+                <a href="http://disk.tinaroot.cn" target="_blank">
+                    <el-card class="box-card">
+                        <div slot="header" class="clearfix">
+                            <span>网盘</span>
+                            <el-button style="float: right; padding: 3px 0" type="text">
+                                跳转
+                            </el-button>
+                        </div>
+                        <div class="text item">
+                            <img src="https://ae01.alicdn.com/kf/H8b39c76b0a05462aac2eec7760a4e91c7.png"/>
                         </div>
                     </el-card>
                 </a>
@@ -38,6 +72,7 @@
 
 <script>
 import axios from 'axios'
+
 export default {
     name: 'Tina',
     created() {
